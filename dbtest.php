4693360fe604018b81075014794292e6907e7baf
kkoo <kkoo@purdue.edu>
2013-10-29 02:12:56 -0400

    Login session update

--- dbtest.php
+++ dbtest.php
@@ -1,6 +1,6 @@
 <?php
 include "DBaccess.php";
 
-function login("ddkdj", "ddddd", "http://www.google.com", "http://www.yahoo.com") {
+login("ddkdj", "ddddd", "http://www.google.com", "http://www.yahoo.com") {
 
 ?>
