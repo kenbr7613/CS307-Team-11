c28ec1645a0273a32ccc27da90b5c3669d2db7c9
kkoo <kkoo@purdue.edu>
2013-10-29 01:00:57 -0400

    DBaccess.php added comments, cleaned code

--- DBaccess.php
+++ DBaccess.php
@@ -1,13 +1,44 @@
 <?php
-//include "/u/u88/bpastine/cs307/apache/DBconfig.php";
 
+//include the DB configuration file
 include "DBconfig.php";
 
+
+/////////////////////////////////////////////////////////
+//
+//Function: dbConnectPDO
+//
+//Author: Ki Hyun Koo
+//
+//Use: Connects to Database Using PDO
+//
+//Arguments: NONE
+//
+//Return: 
+//$pdo		-PDO database object
+//
+/////////////////////////////////////////////////////////
+/*
 function dbConnectPDO() {
 	$pdo = new PDO('mysql:host=$db_host;dbname=db_name', $username, $password);
 	return $pdo;
 }
+*/
 
+
+/////////////////////////////////////////////////////////
+//Function: dbConnect
+//
+//Author: Ki Hyun Koo
+//
+//Use: Connects to Database
+//
+//Arguments: NONE
+//
+//Return: 
+//$db		-database object
+//
+/////////////////////////////////////////////////////////
 function dbConnect() {
 	//$db = mysql_connect($db_host, $db_username, $db_password) or die ('could not connect');
 	
@@ -21,11 +52,35 @@ function dbConnect() {
 	}
 }
 
-//Checks user credentials
-//argument: user, password
-//return 0 fail
-//return 1 pass
+function dbConnectTest() {
+	$db = mysql_connect($db_host, $db_username, $db_password) or die ('could not connect');
+	
+	//$db = mysql_connect('lore.cs.purdue.edu:11394', 'root','cs307team11') or die ('could not connect');
+	mysql_select_db('purduePlannerDB', $db);
+	if (!$db) {
+    		die('Could not connect: ' . mysql_error());
+	}
+	else {
+		return $db;
+	}
+}
 
+/////////////////////////////////////////////////////////
+//Function: checkCred
+//
+//Author: Ki Hyun Koo
+//
+//Use: Checks user's credentials
+//
+//Arguments:
+//$user		-username
+//$pass		-password
+//
+//
+//Return: 
+//0			-if fail
+//1 		-if passed
+/////////////////////////////////////////////////////////
 function checkCred($user, $pass) {
 	$db = dbConnect();
 	$username = mysql_real_escape_string($_POST['$user']); 
@@ -43,6 +98,21 @@ function checkCred($user, $pass) {
 	
 }
 
+/////////////////////////////////////////////////////////
+//Function: Login
+//
+//Author: Ki Hyun Koo
+//
+//Use: Checks credentials of user using username and password then redirects to given url a new session is started with username
+//
+//Arguments:
+//$user		-username
+//$pass		-password
+//$urlSuc	-url to be redirected if login is sucessful
+//$urlFail	-url to be redirected if login fails
+//
+//Return: NONE
+/////////////////////////////////////////////////////////
 function login($user, $pass) {
 	$db = dbConnect();
 	$username = mysql_real_escape_string($user); 
@@ -53,27 +123,14 @@ function login($user, $pass) {
 	$result = mysql_query($sql, $db);
 	
 	if( mysql_num_rows($result) == 1) {
-		session_register($username);
-		session_register($password);
+		session_start();
+		$_SESSION['username'] = $username;
 		header("location:login_suc.php?username=$username");
 	}
 	else {
-		echo "login failed";
+		header("location:login_fail.php?username=$username");
 	}	
 }
-
-
-//test
-/*
-if ( checkCred("testuser", md5("testpass")) ) {
-	echo "correct";
-}
-else {
-	echo "false";
-}
-*/
-//login("testuser", md5("testpass"));
-//login("testuser1", "testpass");
 
 
 ?>
