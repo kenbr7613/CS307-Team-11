97f259e1a4e53e86f393497ce0a7c342ccb89a53
bpastene <bpastene@purdue.edu>
2013-11-10 18:35:53 -0500

    Update editAccount.php

--- editAccount.php
+++ editAccount.php
@@ -1,9 +1,118 @@
 <!DOCTYPE html>
 <html>
     <head>
+		<script>
+		function addClass(){
+			var deps = document.getElementById("dep");
+			var dep = deps.options[deps.selectedIndex].innerHTML;
+			
+			var lvls = document.getElementById("level");
+			var lvl = lvls.options[lvls.selectedIndex].innerHTML;
+			
+			var table = document.getElementById("listOfClasses");
+			var rows = table.rows.length;
+			
+			var row = table.insertRow(rows);
+			var cell1 = row.insertCell(0);
+			var cell2 = row.insertCell(1);
+			
+			//cell1.innerHTML = "<p id=\"" + lvls.options[lvls.selectedIndex].value + "\">" + lvlsdep.concat(lvl) + "</p>";
+			//cell1.innerHTML = "<p>" + lvlsdep.concat(lvl) + "</p>";
+			cell1.innerHTML = dep.concat(lvl);
+			cell1.id = lvls.options[lvls.selectedIndex].value;
+			cell2.innerHTML = "<button type=\"button\" onclick=\"dropClass(this)\">Remove Class</button>";
+		}
+		function dropClass(button) {
+			var table = document.getElementById("listOfClasses");
+			var td = button.parentNode;
+			var tr = td.parentNode;
+			var ind = tr.rowIndex;
+			table.deleteRow(ind);
+		}
+		function setClassValue() {
+			var courses = document.getElementById("courses");
+			var table = document.getElementById("listOfClasses");
+			var rows = table.rows.length;
+			var row;
+			for (var i = 0; i < rows; i++) {
+				row = table.rows[i];
+				courses.value = courses.value + " " + row.cells[0].id;
+			}
+		}
+	</script>
 		<?php
 			include('session_login_check.php');
 			include "DBaccess.php";
+			
+			$colleges = array();
+			$majors = array();
+			$minors = array();
+			$courses = array();
+			$userId = $_SESSION['login'];
+			
+			$db = dbConnect();
+			$sql = sprintf("select DegreeID,RequirementDesc from DegreeRequirements where Type=1 order by RequirementDesc");
+			$result = mysql_query($sql, $db);
+			while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
+				array_push($colleges, array($row[0], $row[1]));
+			}
+			
+			$sql = sprintf("select DegreeID,RequirementDesc from DegreeRequirements where Type=2 order by RequirementDesc");
+			$result = mysql_query($sql, $db);
+			while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
+				array_push($majors, array($row[0], $row[1]));
+			}
+			
+			$sql = sprintf("select DegreeID,RequirementDesc from DegreeRequirements where Type=3 order by RequirementDesc");
+			$result = mysql_query($sql, $db);
+			while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
+				array_push($minors, array($row[0], $row[1]));
+			}
+			
+			$sql = sprintf("select CourseID from UserCoursesCompleted where UserID=%d", $userId);
+			$result = mysql_query($sql, $db);
+			while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
+				$sql = sprintf("select Department,Level,Title from Courses where CourseID=%d", $row[0]);
+				$resultCourse = mysql_query($sql, $db);
+				$rowCourse = mysql_fetch_array($resultCourse);
+				array_push($courses, array($row[0], $rowCourse[0], $rowCourse[1], $rowCourse[2]));
+			}
+			
+			$userInfo;
+			$sql = sprintf("select Username,College1,College2,College3,Major1,Major2,Major3,Minor1,Minor2,Minor3,FirstName,LastName from Users where UserID=%d", $userId);
+			$result = mysql_query($sql, $db);
+			while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
+				$userInfo =$row;
+			}
+			
+			$sql = "select distinct Department from Courses;";
+			$resultD = mysql_query($sql, $db);
+			$array = array();
+			while ($dep = mysql_fetch_array($resultD, MYSQL_BOTH)) {
+				$sql = sprintf("select Level,Title,CourseID from Courses where Department =\"%s\";", $dep[0]);
+				$resultL = mysql_query($sql, $db);
+				$array[$dep[0]] = array();
+				while ($lev = mysql_fetch_array($resultL, MYSQL_BOTH)) {
+					$array[$dep[0]][$lev[0]] = array($lev[0],$lev[1],$lev[2]);
+				}							
+			}
+			echo "<script>\n";
+			echo "function populateNum() {\n";
+			echo "var deps = document.getElementById(\"dep\");\n";
+			echo "var lvls = document.getElementById(\"level\");\n";
+			echo "var dep = deps.options[deps.selectedIndex].value;\n";
+			$keys1 = array_keys($array);
+			foreach ($keys1 as $key1) {
+				printf("if (dep == \"%s\") {\n", $key1);
+				echo "\tlvls.options.length=0;\n";
+				$i = 0;
+				$keys2 = array_keys($array[$key1]);
+				foreach ($keys2 as $key2) {				
+					printf("\tlvls.options[%d]=new Option(\"%s - %s\", \"%s\", false, false);\n", $i++, $array[$key1][$key2][0], $array[$key1][$key2][1], $array[$key1][$key2][2]);
+				}
+				echo "}\n";
+			}
+			echo "}</script>";
 		?>
 		<title>Purdue Planner</title>
 		<style type="text/css">
@@ -93,110 +202,272 @@
 			<h1>&nbsp;</h1>
 			<h1 align="center"><img src="images/purdue_logo.png" width="215" height="80"></h1>
 			<h1 class="STYLE16">Edit Your Account </h1>
-			<form name="form1" method="post" action="settingsChanged.php">
-			<table width="663" height="344" border="2">
+			<form name="form1" method="post" action="settingsChanged.php" onsubmit="setClassValue()">
+			<table width="1000" height="344" border="2">
 				<tr>
-				<td><div align="center" class="STYLE3">Email</div></td>
-				<td><label>
-				<div align="center">
-				<input name="email" type="text" id="email" />
-				</div>
-				</label></td>
+					<td><div align="center" class="STYLE3">Email</div></td>
+					<td><label>
+					<div align="center">
+					<input name="email" type="text" id="email" 
+						<?php
+							printf("value =\"%s\" ", $userInfo[0]);
+						?>
+					/>
+					</div>
+					</label></td>
 				</tr>
 				<tr>
-				<td><div align="center" class="STYLE3">Purdue Planner Password (at least 6 characters) </div></td>
-				<td><label>
-				<div align="center">
-				<input name="passwd" type="password" id="passwd" />
-				</div>
-				</label></td>
+					<td height="28"><div align="center"><span class="STYLE3">First Name </span></div></td>
+					<td><div align="center">
+					<label>
+					<input name="fname" type="text" id="fname" 
+						<?php
+							printf("value =\"%s\" ", $userInfo[10]);
+						?>
+					/>
+					</label>
+					</div></td>
 				</tr>
 				<tr>
-				<td><div align="center" class="STYLE3">Confirm Password </div></td>
-				<td><label>
-				<div align="center">
-				<input name="passwd2" type="password" id="passwd2" />
-				</div>
-				</label></td>
+					<td height="30"><div align="center"><span class="STYLE3">Last Name </span></div></td>
+					<td><div align="center">
+					<label>
+					<input name="lname" type="text" id="lname" 
+						<?php
+							printf("value =\"%s\" ", $userInfo[11]);
+						?>
+					/>
+					</label>
+					</div></td>
 				</tr>
 				<tr>
-				<td><div align="center" class="STYLE3">First Name </div></td>
-				<td><label>
-				<div align="center">
-				<input name="fname" type="text" id="fname" />
-				</div>
-				</label></td>
+					<td><div align="center" class="STYLE3">Purdue Planner Password <br />(leave blank if you want your password unchanged) </div></td>
+					<td><label>
+					<div align="center">
+					<input name="passwd" type="password" id="passwd" />
+					</div>
+					</label></td>
+					</tr>
+				<tr>
+					<td><div align="center" class="STYLE3">Confirm Password </div></td>
+					<td><label>
+					<div align="center">
+					<input name="passwd2" type="password" id="passwd2" />
+					</div>
+					</label></td>
 				</tr>
 				<tr>
-				<td><div align="center" class="STYLE3">Last Name </div></td>
-				<td><label>
-				<div align="center">
-				<input name="lname" type="text" id="lname" />
-				</div>
-				</label></td>
+					<td height="38"><div align="center" class="STYLE3">Major(s) * </div></td>
+					<td>
+						<div align="center">
+							<table border="0">
+								<tr>
+									College:
+									<select name="college1">
+										<option>N/A</option>
+										<?php
+											for ($i = 0; $i < sizeof($colleges); $i++) {
+												printf("<option value=\"%s\" ", $colleges[$i][0]);
+												if ($userInfo[1] == $colleges[$i][0]) {
+													printf("selected");
+												}
+												printf(">%s</option>", $colleges[$i][1]);
+											}
+										?>
+									</select>
+									Major:
+									<select name="major1">
+										<option>N/A</option>
+										<?php
+											for ($i = 0; $i < sizeof($majors); $i++) {
+												printf("<option value=\"%s\" ", $majors[$i][0]);
+												if ($userInfo[4] == $majors[$i][0]) {
+													printf("selected");
+												}
+												printf(">%s</option>", $majors[$i][1]);
+											}
+										?>
+									</select>
+								</tr>
+							</table>
+							<table border="0">
+								<tr>
+									College:
+									<select name="college2">
+										<option>N/A</option>
+										<?php
+											for ($i = 0; $i < sizeof($colleges); $i++) {
+												printf("<option value=\"%s\" ", $colleges[$i][0]);
+												if ($userInfo[2] == $colleges[$i][0]) {
+													printf("selected");
+												}
+												printf(">%s</option>", $colleges[$i][1]);
+											}
+										?>
+									</select>
+									Major:
+									<select name="major2">
+										<option>N/A</option>
+										<?php
+											for ($i = 0; $i < sizeof($majors); $i++) {
+												printf("<option value=\"%s\" ", $majors[$i][0]);
+												if ($userInfo[5] == $majors[$i][0]) {
+													printf("selected");
+												}
+												printf(">%s</option>", $majors[$i][1]);
+											}
+										?>
+									</select>
+								</tr>
+							</table>
+							<table border="0">
+								<tr>
+									College:
+									<select name="college3">
+										<option>N/A</option>
+										<?php
+											for ($i = 0; $i < sizeof($colleges); $i++) {
+												printf("<option value=\"%s\" ", $colleges[$i][0]);
+												if ($userInfo[3] == $colleges[$i][0]) {
+													printf("selected");
+												}
+												printf(">%s</option>", $colleges[$i][1]);
+											}
+										?>
+									</select>
+									Major:
+									<select name="major3">
+										<option>N/A</option>
+										<?php
+											for ($i = 0; $i < sizeof($majors); $i++) {
+												printf("<option value=\"%s\" ", $majors[$i][0]);
+												if ($userInfo[6] == $majors[$i][0]) {
+													printf("selected");
+												}
+												printf(">%s</option>", $majors[$i][1]);
+											}
+										?>
+									</select>
+								</tr>
+							</table>
+						</div>
+					</td>
 				</tr>
 				<tr>
-				<td><div align="center" class="STYLE3">College</div></td>
-				<td><label>
-				<div align="center">
-				<select name="select">
-				</select>
-				</div>
-				</label></td>
+					<td height="38"><div align="center" class="STYLE3">Minor(s) * </div></td>
+					<td><div align="center">
+					<table border="0">
+						<tr>
+							Minor:
+							<select name="minor1">
+								<option>N/A</option>
+								<?php
+									for ($i = 0; $i < sizeof($minors); $i++) {
+										printf("<option value=\"%s\" ", $minors[$i][0]);
+										if ($userInfo[7] == $minors[$i][0]) {
+											printf("selected");
+										}
+										printf(">%s</option>", $minors[$i][1]);
+									}
+								?>
+							</select>
+						</tr>
+					</table>
+					<table border="0">
+						<tr>
+							Minor:
+							<select name="minor2">
+								<option>N/A</option>
+								<?php
+									for ($i = 0; $i < sizeof($minors); $i++) {
+										printf("<option value=\"%s\" ", $minors[$i][0]);
+										if ($userInfo[8] == $minors[$i][0]) {
+											printf("selected");
+										}
+										printf(">%s</option>", $minors[$i][1]);
+									}
+								?>
+							</select>
+						</tr>
+					</table>
+					<table border="0">
+						<tr>
+							Minor:
+							<select name="minor3">
+								<option>N/A</option>
+								<?php
+									for ($i = 0; $i < sizeof($minors); $i++) {
+										printf("<option value=\"%s\" ", $minors[$i][0]);
+										if ($userInfo[9] == $minors[$i][0]) {
+											printf("selected");
+										}
+										printf(">%s</option>", $minors[$i][1]);
+									}
+								?>
+							</select>
+						</tr>
+					</table>
+					</div></td>
 				</tr>
 				<tr>
-				<td height="38"><div align="center" class="STYLE3">Major(s) * </div></td>
-				<td><label>
-				<div align="center">
-				<select name="select2">
-				</select>
-				<select name="select3">
-				<option>N/A</option>
-				</select>
-				<select name="select4">
-				<option>N/A</option>
-				</select>
-				</div>
-				</label></td>
-				</tr>
-				<tr>
-				<td height="38"><div align="center" class="STYLE3">Minor(s) * </div></td>
-				<td><div align="center">
-				<label>
-				<select name="select5">
-				<option>N/A</option>
-				</select>
-				</label>
-				<label>
-				<select name="select6">
-				<option>N/A</option>
-				</select>
-				</label>
-				<label>
-				<select name="select7">
-				<option>N/A</option>
-				</select>
-				</label>
-				</div></td>
-				</tr>
-				<tr>
-				<td height="51" colspan="2"><div align="center">
-				<label>
-				<input type="submit" name="Submit" value="Submit" />
-				</label>
-				<label>
-				<input type="reset" name="Submit2" value="Reset" />
-				</label>
-			</form>
-				<form action="home.php"><input type="submit" value="Go Back"></form>
-				</div>
-				<label></label>
-				<div align="center">
-				<label></label>
-				</div>          
-				<div align="right"></div><label></label></td>
+					<td height="51" colspan="2"><div align="center">
+					<label>
+					<input type="submit" name="Submit" value="Submit" />
+					</label>
+					<label>
+					<input type="reset" name="Submit2" value="Reset" />
+					</label>
+					</form>
+					<form action="home.php"><input type="submit" value="Go Back"></form>
+					</div>
+					<label></label>
+					<div align="center">
+					<label></label>
+					</div>          
+					<div align="right"></div><label></label></td>
 				</tr>
 			</table>
+			<br />
+			<br />
+			<table border="0">
+				<tr>
+					<td>
+						Department:
+						<select id="dep" onchange="populateNum()">
+							<option value="null">--</option>
+							<?php
+								$sql = "select distinct Department from Courses;";
+								$result = mysql_query($sql, $db);
+								while ($dep = mysql_fetch_array($result, MYSQL_BOTH)) {
+									$s = $dep[0];
+									echo "<option value=\"$s\">$s</option>";
+								}
+							?>					
+						</select>
+					</td>
+					<td>
+						Level: 
+						<select id="level">
+							<option value="null">--</option>
+						</select>
+					</td>
+				</tr>
+			</table>
+			<button type="button" onclick="addClass()">Add Class</button>
+			<input type="hidden" name="courses" value="" id="courses">
+			<input type="submit" value="Submit" />
+		</form>
+		<br />
+		<br />
+		<table id="listOfClasses" border="0">
+			<tbody>
+				<?php
+					for ($i = 0; $i < sizeof($courses); $i++) {
+						printf("<tr><td id=\"%d\">%s%s - %s</td><td><button type=\"button\" onclick=\"dropClass(this)\">Remove Class</button</td></tr>", $courses[$i][0], $courses[$i][1], $courses[$i][2], $courses[$i][3]);
+					}
+				?>
+			</tbody>
+		</table>
 		</div>
 	</body>
 </html>
