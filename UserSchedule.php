2c1e2128377f0c1d0a9e0548de5e4703f2bb5aec
kkoo <kkoo@purdue.edu>
2013-11-11 00:26:43 -0500

    update calendar view and 'cart' feature; viewSchedule.php, UserSchedule.php

--- UserSchedule.php
+++ UserSchedule.php
@@ -29,11 +29,13 @@ class UserSchedule {
 		//get courseOfferingID from database
 		$offeringID = $this->getcourseOfferingID($crn);
 		if($offeringID < 1) {
+			echo " Course does not exist ";
 			return -1;
 		}
 		
 		//check if the user is already taking the course;
 		if( $this->isTaking($crn) ) {
+			echo " Course is already Registered ";
 			return -1;
 		}
 		
@@ -43,7 +45,8 @@ class UserSchedule {
 			return -1;
 		}
 		
-		if(!$this->timeAvailable($courseTime['StartTime'], $courseTime['EndTime'], $couseTime['StartDate'], $couseTime['EndDate']) ) {
+		if(!$this->timeAvailable($courseTime['StartTime'], $courseTime['EndTime'], $courseTime['StartDate'], $courseTime['EndDate'], $courseTime['Days']) ) {
+			echo " TIME CONFLICT ";
 			return -1;
 		}
 		
@@ -78,13 +81,21 @@ class UserSchedule {
 			return -1;
 		}
 		else {
-			if( ($index = array_search($offeringID, $this->courseOfferingIDs)) != false ) {
-				print_r($this->courseOfferingIDs);
-				echo "unset";
-				echo $index;
-    			unset($this->couseOfferingIDs[$index]);
-    			print_r($this->courseOfferingIDs);
+			$temp = $this->courseOfferingIDs;
+			if( ($index = array_search($offeringID, $temp)) != false ){
+				unset($temp[$index]);
+				$this->courseOfferingIDs = $temp;
 			}
+			/*
+			if( ($index = array_search($offeringID, $this->courseOfferingIDs)) != false ) {
+				//print_r($this->courseOfferingIDs);
+				//echo "unset";
+				//echo $index;
+				
+    			unset($this->couseOfferingIDs[$index]);
+    			//print_r($this->courseOfferingIDs);
+			}
+			*/
 			return 1;
 		}
 	}
@@ -98,7 +109,26 @@ class UserSchedule {
 		return in_array($offeringID, $this->courseOfferingIDs);
 	}
 	
-	public function timeAvailable($startTime, $finnishTime, $startDate, $endDate) {
+	public function timeAvailable($startTime, $finnishTime, $startDate, $endDate, $days) {
+		$array = $this->courseOfferingIDs;
+		foreach($array as $value) {
+			$result = $this->getCourseDetail($value);
+			if($result == -1) {
+				//error
+				return false;
+			}
+			if( ($result['Start'] >= $startTime) && ($result['Start'] <= $finnishTime) ){
+				if( similar_text($days, $result['Days']) > 0 ){
+					return false;
+				}
+			}
+			else if( ($result['End'] >= $startTime) && ($result['End'] <= $finnishTime) ){
+				if (similar_text($days, $result['Days']) > 0 ){
+					return false;
+				}
+			}
+			
+		}
 		return true;
 	}
 	/*
@@ -108,42 +138,326 @@ class UserSchedule {
 				}
 				*/
 	
-	public function listSchedule() {
-		//echo "<table border = '1'>";
-		$array = $this->courseOfferingIDs;
-		$tableRow = array(' ', 'CRN', 'Title', 'Credits', 'StartTime', 'EndTime', 'Room', 'Instructor');
-		echo $this->drawTableRow($tableRow, "white");
 	
+	public function drawWeeklySchedule() {
+		$courses = array();
+		//get all course info
+		$array = $this->courseOfferingIDs;
 		foreach($array as $value) {
-			echo $this->createRow($value, "white");
+			$result = $this->getCourseDetail($value);
+			if($result == -1) {
+				//error
+				return;
+			}
+			$courses[] = $result;
 		}
-		//echo "</table>";
+	
+		//create array of days
+		$daysArray = array();
+		for($i=0; $i < 7; $i++) {
+			$daysArray[] = array();
+		}
+		
+		//create array of times
+		$timesArray = array();
+		for($i = 0; $i < (4*23); $i++) {
+			$timesArray[] = $daysArray;
+		}
+		
+		foreach($courses as $value) {
+			$start = $value['Start'];
+			$end = $value['End'];
+			$days = $value['Days'];
+			
+			//get start, end; hours and minutes
+			list($startHour, $startMinute) = explode(":", $start);
+			list($endHour, $endMinute) = explode(":", $end);
+			$startHour = intval($startHour);
+			$startMinute = intval($startMinute);
+			$endHour = intval($endHour);
+			$endMinute = intval($endMinute);
+			
+			//convert hours and minutes to timesArray index
+			$startMinuteIndex;
+			if($startMinute < 20) { $startMinuteIndex = 0; }
+			else if($startMinute < 30) { $startMinuteIndex = 1; }
+			else if($startMinute < 50) { $startMinuteIndex = 2; }
+			else { $startMinuteIndex = 3; }
+			
+			$endMinuteIndex;
+			if($endMinute <= 20) { $endMinuteIndex = 0; }
+			else if($endMinute <= 30) { $endMinuteIndex = 1; }
+			else if($endMinute <= 50) { $endMinuteIndex = 2; }
+			else { $endMinuteIndex = 3; }
+			
+			
+			$startIndex = $startHour * 4 + $startMinuteIndex;
+			$endIndex = $endHour * 4 + $endMinuteIndex;
+			$timeSpan = $endIndex - $startIndex + 1;
+						
+			//convert days to indicies
+			$daysIndices = array();
+			if( strstr($days, 'TBA') != false) {  }
+			else {
+				if( strstr($days, 'M') != false) { $daysIndices[] = 0; }
+				if( strstr($days, 'T') != false) { $daysIndices[] = 1; }
+				if( strstr($days, 'W') != false) { $daysIndices[] = 2; }
+				if( strstr($days, 'R') != false) { $daysIndices[] = 3; }
+				if( strstr($days, 'F') != false) { $daysIndices[] = 4; }
+			}
+			
+			//populate times array
+			foreach($daysIndices as $ind) {
+				$timesArray[$startIndex][$ind] = $value;
+				$timesArray[$startIndex][$ind]['TimeSpan'] = $timeSpan;
+			}
+		}		
+		
+		//draw table!!!!
+		$borrow = array(0,0,0,0,0,0,0);
+		echo "<table id=\"schedule\"border = \"1\">";
+		echo "<tr><th></th><th>Monday</th><th>Tuesday</th><th>Wednesday</th><th>Thursday</th><th>Friday</th><th>Saturday</th><th>Sunday</th></tr>";
+		for($timeInd = 32; $timeInd < 72; $timeInd++) {
+			//time
+			if($timeInd % 4 == 0 ) {
+				$realTime = strval(floor($timeInd / 4));
+				$realTime .= ":00";
+			}
+			else{
+				$realTime = null;
+			}
+			//borrowed row
+			for($i = 0; $i < 7; $i++) {
+				$span = $timesArray[$timeInd][$i]['TimeSpan'];
+				if($span > 1) {
+					$borrow[$i] += ($span);
+				}
+			}
+				
+			echo $this->drawWeeklyScheduleRow( $realTime, $timesArray[$timeInd], $borrow, $timeInd );
+			
+			//decrement borrow
+			for($i = 0; $i < 7; $i++) {
+				if($borrow[$i] > 0) {
+					$borrow[$i]--;
+				}
+			}
+		}
+		echo "</table>";
 	}
 	
-	public function createRow($offeringID, $color) {
+
+	
+	public function drawWeeklyScheduleRow($realTime, $array, $borrow, $timeInd) {		
+		$string = "<tr>";
+		//print time if minute is zero
+		if($realTime != null) {
+			$string .= "<td rowspan = \"4\">$realTime</td>";
+		}
+		else {
+			//$string .= "<td></td>";
+		}
+		
+		
+		for($i = 0; $i < 7; $i++) {
+			if($array[$i] != null){
+				$span = $array[$i]['TimeSpan'];
+				$string .= "<td id=\"{$timeInd}:{$i}\" rowspan = \"{$span}\">";
+				$string .= "{$array[$i]['Department']} {$array[$i]['Level']}";
+				$string .= "</td>";
+			}
+			else {
+				if($borrow[$i] > 0) {
+					//do nothing
+				}
+				else {
+					//$string .= "<td> &nbsp;	</td>";
+					$string .= "<td id=\"{$timeInd}:{$i}\"></td>";
+				}
+			}
+			
+		}
+		/*
+		foreach($array as $value) {
+			if($value != null){
+				$string .= "<td rowspan = {$value['TimeSpan']}>";
+			}
+			else {
+				$string .= "<td>";
+			}
+			$string .= "{$value['Department']} {$value['Level']}";
+			$string .= "</td>";
+		}
+		*/
+		$string .= "</tr>";
+		return $string;
+	}
+	
+	private function getCourseDetail($offeringID) {
 		$sql = "select * from CourseOfferings where CourseOfferingID = $offeringID";
 		$result = mysql_query($sql, $this->db);
 		if(!$result) {
 			return -1;
 		}
-		else {
-			$row = mysql_fetch_array($result, MYSQL_ASSOC);
-			
-			//get additional info the course
-			$courseID = $row['CourseID'];
-			$courseinfo = $this->getCourseInfo($courseID);
-			
-			$tableRow = array(
-			"<input type=\"checkbox\" name=\"course\" value=\"{$row['CRN']}\">", 
-			$row["CRN"], 
-			$courseinfo["Title"],
-			$courseinfo["Credits"],
-			$row["StartTime"], 
-			$row["EndTime"],
-			$row["Room"],
-			$row["Instructor"] );
-			return $this->drawTableRow($tableRow, $color);
+		
+		//department level start end location
+		$row = mysql_fetch_array($result, MYSQL_ASSOC);
+		
+		//get start and end date
+		$days = $row['Days'];
+		$startTime = $row['StartTime'];
+		$endTime = $row['EndTime'];
+		
+		
+		//get level and department
+		$courseID = $row['CourseID'];
+		$sql = "select * from Courses where CourseID = $courseID";
+		$result = mysql_query($sql, $this->db);
+		if(!$result) {
+			return -1;
 		}
+		$row = mysql_fetch_array($result, MYSQL_ASSOC);
+		$department = $row['Department'];
+		$level = $row['Level'];
+		
+		$courseInfo = array("Department"=>$department, "Level"=>$level, "Start"=>$startTime, "End"=>$endTime, "Days"=>$days);
+		
+		return $courseInfo;
+	}
+
+	public function listSchedule() {
+		//echo "<table border = '1'>";
+		$array = $this->courseOfferingIDs;
+		$tableRow = array(' ', 'CRN', 'Title', 'Credits', 'StartTime', 'EndTime', 'Days', 'Room', 'Instructor');
+		echo $this->drawTableRow($tableRow, "white");
+	
+		foreach($array as $value) {
+			$return = $this->createListScheduleRow($value, "white");
+			if($return == -1) {
+				//error
+			}
+			else {
+				echo $return;
+			}
+		}
+		//echo "</table>";
+	}
+	
+	public function createListScheduleRow($offeringID, $color) {
+		$sql = "select * from CourseOfferings where CourseOfferingID = $offeringID";
+		$result = mysql_query($sql, $this->db);
+		if(!$result) {
+			return -1;
+		}
+		
+		$row = mysql_fetch_array($result, MYSQL_ASSOC);
+		
+		//get additional info the course
+		$courseID = $row['CourseID'];
+		$courseinfo = $this->getCourseInfo($courseID);
+		if($courseinfo == -1) {
+			//error
+			return -1;
+		}
+		
+		$department = $courseinfo['Department'];
+		$level = $courseinfo['Level'];
+		$deptLevel = "$department $level";
+		
+		$tableRow = array(
+		"<input type=\"checkbox\" name=\"course\" value=\"{$row['CRN']}\">", 
+		$row["CRN"], 
+		$deptLevel,
+		$courseinfo["Credits"],
+		$row["StartTime"], 
+		$row["EndTime"],
+		$row["Days"],
+		$row["Room"],
+		$row["Instructor"] );
+		return $this->drawTableRow($tableRow, $color);
+		
+	}
+	public function courseExists($crn){
+		$offeringID = $this->getcourseOfferingID($crn);
+		$sql = "select * from CourseOfferings where CourseOfferingID = $offeringID";
+		$result = mysql_query($sql, $this->db);
+		if(!$result) {
+			//error
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+	
+	public function createCandidateBox($crn){
+		$offeringID = $this->getcourseOfferingID($crn);
+		$sql = "select * from CourseOfferings where CourseOfferingID = $offeringID";
+		$result = mysql_query($sql, $this->db);
+		if(!$result) {
+			//error
+			return -1;
+		}
+		
+		$row = mysql_fetch_array($result, MYSQL_ASSOC);
+		
+		//get additional info the course
+		$courseID = $row['CourseID'];
+		$startTime = $row['StartTime'];
+		$endTime = $row['EndTime'];
+		$days = $row['Days'];
+		$deptLevel = $this->getDeptLevel($courseID);
+		
+		//calculate row and column
+		list($startHour, $startMinute) = explode(":", $startTime);
+		list($endHour, $endMinute) = explode(":", $endTime);
+		$startHour = intval($startHour);
+		$startMinute = intval($startMinute);
+		$endHour = intval($endHour);
+		$endMinute = intval($endMinute);
+		
+		
+		$startMinuteIndex;
+		if($startMinute < 20) { $startMinuteIndex = 0; }
+		else if($startMinute < 30) { $startMinuteIndex = 1; }
+		else if($startMinute < 50) { $startMinuteIndex = 2; }
+		else { $startMinuteIndex = 3; }
+		
+		$endMinuteIndex;
+		if($endMinute <= 20) { $endMinuteIndex = 0; }
+		else if($endMinute <= 30) { $endMinuteIndex = 1; }
+		else if($endMinute <= 50) { $endMinuteIndex = 2; }
+		else { $endMinuteIndex = 3; }
+		
+		$startIndex = $startHour * 4 + $startMinuteIndex;
+		$endIndex = $endHour * 4 + $endMinuteIndex;
+		$timeSpan = $endIndex - $startIndex + 1;
+		
+		
+		$columns = array();
+		if( strstr($days, 'M') != false) { $columns[] = 0; }
+		if( strstr($days, 'T') != false) { $columns[] = 1; }
+		if( strstr($days, 'W') != false) { $columns[] = 2; }
+		if( strstr($days, 'R') != false) { $columns[] = 3; }
+		if( strstr($days, 'F') != false) { $columns[] = 4; }
+
+		
+		//$row = ($startHour - 8) * 4 + $startMinuteIndex;
+		$rowStart = $startIndex;
+		$rowEnd = $endIndex;
+		
+		$jsArray = "[";
+		foreach($columns as $list){
+			$jsArray .= strval($list);
+			$jsArray .= strval(",");
+		}
+		if(sizeof($columns) > 0) {
+			$jsArray = substr($jsArray, 0, -1);
+		}
+		$jsArray .= "]";
+		
+		$string = "<table border = \"1\"><tr onClick=\"highlight({$rowStart},{$rowEnd}, {$jsArray})\" id=\"{$startTime}.{$endTime}.{$days}\"><td><input type=\"checkbox\" name=\"cartCourse\" value=\"{$crn}\"></td><td> $deptLevel </td></tr></table>";
+		return $string;
 	}
 	
 	private function drawTableRow($array, $color) {
@@ -170,6 +484,21 @@ class UserSchedule {
 			return mysql_fetch_array($result, MYSQL_ASSOC);
 		}
 	}
+	private function getDeptLevel($courseID) {
+		$sql = "select * from Courses where CourseID = $courseID";
+		$result = mysql_query($sql, $this->db);
+		if(!$result) {
+			return -1;
+		}
+		$row = mysql_fetch_array($result, MYSQL_ASSOC);
+		$department = $row['Department'];
+		$level = $row['Level'];
+		
+		$returnVal = $department;
+		$returnVal .= " ";
+		$returnVal .=$level;
+		return $returnVal;
+	}
 	
 	private function getcourseOfferingID($crn) { 
 		$sql = "select CourseOfferingID from CourseOfferings where CRN = $crn";
@@ -184,7 +513,7 @@ class UserSchedule {
 	}
 	
 	private function getCourseTime($offeringID) {
-		$sql = "select StartTime, EndTime, StartDate, EndDate from CourseOfferings where CourseOfferingID = $offeringID";		 
+		$sql = "select StartTime, EndTime, StartDate, EndDate, Days from CourseOfferings where CourseOfferingID = $offeringID";		 
 		$result = mysql_query($sql, $this->db);
 		if(!$result) {
 			return -1;
@@ -195,11 +524,42 @@ class UserSchedule {
 				"StartTime" => $row["StartTime"],
 				"EndTime" => $row["EndTime"],
 				"StartDate" => $row["StartDate"],
-				"EndDate" => $row["EndDate"]
+				"EndDate" => $row["EndDate"],
+				"Days" => $row["Days"]
 				);
 			}
 		}
 		
 	}
+			/*		
+		
+		$courseDuration = ($endHour - $startHour) * 60 + ($endMinute - $startMinute);
+			$courseDurationInt = floor($courseDuration);
+			$timeSpan = ($courseDurationInt * 4) + ($startMinuteIndex + 1) + ($endMinuteIndex + 1);
+			
+			
+		$timeMonday = times; 
+		$days = array('Monday'=>$times, 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+		//echo $this -> drawTableRow($tableRow, "white");
+		
+		///create arrays
+		$monday = array(); $tuesday = array(); $wednesday = array(); $thursday = array(); $friday = array(); 
+		
+		
+		$times = array('8:00'=>'0', '8:20'=>'0', '8:30'=>'0', '8:50'=>'0',
+						'9:00'=>'0', '9:20'=>'0', '9:30'=>'0', '9:50'=>'0',
+						'10:00'=>'0', '10:20'=>'0', '10:30'=>'0', '10:50'=>'0',
+						'11:00'=>'0', '11:20'=>'0', '11:30'=>'0', '11:50'=>'0',
+						'12:00'=>'0', '12:20'=>'0', '12:30'=>'0', '12:50'=>'0',
+						'13:00'=>'0', '13:20'=>'0', '13:30'=>'0', '13:50'=>'0',
+						'14:00'=>'0', '14:20'=>'0', '14:30'=>'0', '14:50'=>'0',
+						'15:00'=>'0', '15:20'=>'0', '15:30'=>'0', '15:50'=>'0',
+						'16:00'=>'0', '16:20'=>'0', '16:30'=>'0', '16:50'=>'0',
+						'17:00'=>'0', '17:20'=>'0', '17:30'=>'0', '17:50'=>'0',
+						'18:00'=>'0', '18:20'=>'0', '18:30'=>'0', '18:50'=>'0',
+						'19:00'=>'0', '19:20'=>'0', '19:30'=>'0', '19:50'=>'0',
+						'20:00'=>'0', '20:20'=>'0', '20:30'=>'0', '20:50'=>'0',
+						'21:00'=>'0', '21:20'=>'0', '21:30'=>'0', '21:50'=>'0');
+		*/
 }
 ?>
--- viewSchedule.php
+++ viewSchedule.php
@@ -1,5 +1,8 @@
 <?php
 include('session_login_check.php');
+?>
+
+<?php
 include('UserSchedule.php');
 echo "<br>";
 $userid = $_SESSION['login'];
@@ -9,6 +12,7 @@ $schedule = new UserSchedule($userid);
 
 //form action
 if($_SERVER["REQUEST_METHOD"] == "POST") {
+	
 	//drop a course
 	if( isset($_POST['drop']) && !isset($_POST['course']) ) {
 		echo "ERROR: No course selected\n";
@@ -17,20 +21,59 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 		$return = $schedule->dropCourse($_POST['course']);
 		if($return > 0) {
 			echo "successfuly dropped {$_POST['course']}\n";
+			$schedule = null;
+			$schedule = new UserSchedule($userid);
 			//$schedule = new UserSchedule($userid);
+			
 		}
 		else {
 			echo "ERROR: dropping course {$_POST['course']}\n";
 		}
 	}
+	//reg from cart
+	if( isset($_POST['regCandidate']) && !isset($_POST['cartCourse']) ) {
+		echo "ERROR: No course selected\n";
+	}
+	else if( isset($_POST['regCandidate']) && isset($_POST['cartCourse']) ) {
+		/*
+		if( !($schedule -> courseExists($_POST['cartCourse'])) ){
+			echo "ERROR: Course does not exist";
+		}
+		else if( $schedule -> isTaking($_POST['cartCourse']) ) {
+			echo "ERROR: Course already registered";
+		}
+		*/
+		$return = $schedule->addCourse($_POST['cartCourse']);
+		if($return > 0) {
+			echo "successfuly added {$_POST['cartCourse']}\n";
+		}
+		else {
+			echo "ERROR: adding course {$_POST['cartCourse']}\n";
+		}
+	}
 	
+	//clear candidates
+	if( isset($_POST['clear']) ) {
+		$_SESSION['courseCandidate'] = null;
+	}
 	//add a candidate
 	if( isset($_POST['candidate']) && ($_POST['candidate'] != '') ) {
-		if( isset( $_SESSION['courseCandidate'] ) ) {
-			$candidate = $_SESSION['courseCandidate'];
+		if( !($schedule -> courseExists($_POST['candidate'])) ){
+			echo "ERROR: Course does not exist";
 		}
-		$candidate[] = $_POST['candidate'];
-		$_SESSION['courseCandidate'] = $candidate;
+		else if( $schedule -> isTaking($_POST['candidate']) ) {
+			echo "ERROR: Course already registered";
+		}
+		else if( isset($_SESSION['courseCandidate']) && in_array($_POST['candidate'], $_SESSION['courseCandidate']) ){
+			echo "ERROR: Course already in cart";
+		}
+		else {
+			if( isset( $_SESSION['courseCandidate'] ) ) {
+				$candidate = $_SESSION['courseCandidate'];
+			}
+			$candidate[] = $_POST['candidate'];
+			$_SESSION['courseCandidate'] = $candidate;
+		}
 	}
 	
 	
@@ -48,6 +91,36 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 
+
+<script>
+
+function highlight(rowStart, rowEnd, colArray){
+		
+		var col;
+		for (var i=0;i<=colArray.length;i++) {
+			col=colArray[i];
+			for(var j=rowStart; j <= rowEnd; j++) {
+				document.getElementById(j + ":" + col).style.backgroundColor="red";
+			}
+		}
+	
+}
+</script>
+
+<style>
+th{
+	width: 70px;
+}
+td
+{
+height:25px;
+}
+</style>
+
+
+<div id="container" style="width:100%">
+
+<div id="listRegistration" style="width:60%;float:left">
 <!--form start-->
 <form name="listSchedule" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
 
@@ -65,10 +138,59 @@ Register a Course
 <button type ="submit">Submit</button>
 <button type ="reset">Reset</button>
 <hr>
+</div>
+</form>
 
+
+<!---div cart START -->
+<div id="cart" style="width:39%;float:right;border:1px solid black">
+	<form name="cart" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+	Cart
+	<?php
+	if( isset( $_SESSION['courseCandidate'] ) ) {
+		$candidate = $_SESSION['courseCandidate'];
+		foreach($candidate as $value) {
+			$return = $schedule->createCandidateBox($value);
+			if($return == -1) {
+				echo "Course does not exist";
+				print_r($_SESSION['courseCandidate']);
+			}
+			else {
+				echo $return;
+			}
+		}
+	}
+	?>
+	<input type="submit" value="Register selected" name="regCandidate">
+	<input type="submit" value="Clear cart" name="clear">
+	
+	<br>
+	Add a Course to Cart
+	<br>
+	<input type="text" name="candidate">
+	<br>
+	<button type ="submit">Submit</button>
+	<button type ="reset">Reset</button>
+	</form>
+</div>
+
+<!---div cart END -->
+
+<div id="weeklySchedule" style="width:100%;clear:both">
+<?php $schedule -> drawWeeklySchedule(); ?>
+</div>
+
+
+
+</div>
+<hr>
+<form name ="printform">
+<input type="button" onClick="window.print()" value="Print">
+</form>
+<!----
 Sample Registration
 <table border = '1'>
-<?php
+<?php /*
 $schedule->listSchedule();
 if( isset( $_SESSION['courseCandidate'] ) ) {
 	$candidate = $_SESSION['courseCandidate'];
@@ -77,8 +199,12 @@ if( isset( $_SESSION['courseCandidate'] ) ) {
 	}
 }
 
-?>
+*/?>
+
+<!-------
 </table>
+
+
 <input type ="radio" name="drop" value="drop">Delete Selected Course
 <br>
 Add a Course
@@ -86,6 +212,6 @@ Add a Course
 <br>
 <button type ="submit">Submit</button>
 <button type ="reset">Reset</button>
+---->
 
-</form>
 
