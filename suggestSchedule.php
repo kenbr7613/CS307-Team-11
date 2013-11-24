7680ad0231fa41112f9ab69ca2f65aeffc58d38f
bpastene <bpastene@purdue.edu>
2013-11-24 12:58:17 -0500

    Update suggestSchedule.php

--- suggestSchedule.php
+++ suggestSchedule.php
@@ -1,3 +1,6 @@
+<?php
+	include('session_login_check.php');
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -94,15 +97,24 @@
 			var table = document.getElementById("listOfClasses");
 			var rows = table.rows.length;
 			
-			var row = table.insertRow(rows);
-			var cell1 = row.insertCell(0);
-			var cell2 = row.insertCell(1);
+			// make sure the class hasn't already been added
+			var alreadyThere = 0;
+			for (var i = 0; i < rows; i++) {
+				var id = table.rows[i].cells[0].id;
+				if (id == lvls.options[lvls.selectedIndex].value) {
+					alreadyThere = 1;
+				}
+			}
 			
-			//cell1.innerHTML = "<p id=\"" + lvls.options[lvls.selectedIndex].value + "\">" + lvlsdep.concat(lvl) + "</p>";
-			//cell1.innerHTML = "<p>" + lvlsdep.concat(lvl) + "</p>";
-			cell1.innerHTML = dep.concat(lvl);
-			cell1.id = lvls.options[lvls.selectedIndex].value;
-			cell2.innerHTML = "<button type=\"button\" onclick=\"dropClass(this)\">Remove Class</button>";
+			if (alreadyThere == 0) {
+				var row = table.insertRow(rows);
+				var cell1 = row.insertCell(0);
+				var cell2 = row.insertCell(1);
+				
+				cell1.innerHTML = dep.concat(lvl);
+				cell1.id = lvls.options[lvls.selectedIndex].value;
+				cell2.innerHTML = "<button type=\"button\" onclick=\"dropClass(this)\">Remove Class</button>";
+			}
 		}
 		function dropClass(button) {
 			var table = document.getElementById("listOfClasses");
@@ -112,6 +124,8 @@
 			table.deleteRow(ind);
 		}
 		function setWantedValue() {
+			setSchedValue();
+			
 			var courses = document.getElementById("wantedClasses");
 			var table = document.getElementById("listOfClasses");
 			var rows = table.rows.length;
@@ -121,40 +135,184 @@
 				courses.value = courses.value + " " + row.cells[0].id;
 			}
 		}
+		function addRange() {
+			var table = document.getElementById("schedPref");
+			var rows = table.rows;
+			var row = table.insertRow(rows.length);
+			
+			var cell1 = row.insertCell(0);
+			var cell2 = row.insertCell(1);
+			var cell3 = row.insertCell(2);
+			var cell4 = row.insertCell(3);
+			var cell5 = row.insertCell(4);
+			var cell6 = row.insertCell(5);
+			cell1.innerHTML = "No class between:";
+			
+			var select1 = document.createElement("select");
+			for (var i = 1; i < 13; i++) {
+				var opt = document.createElement("option");
+				opt.innerHTML = i;
+				if (i == 12) {
+					opt.value = 0;
+				} else {
+					opt.value = i;
+				}
+				select1.appendChild(opt);
+			}
+			
+			var select2 = document.createElement("select");
+			for (var i = 0; i <= 55; i = i+5) {
+				var opt = document.createElement("option");
+				opt.innerHTML = i;
+				opt.value = i;
+				select2.appendChild(opt);
+			}
+			
+			var select3 = document.createElement("select");
+			var opt = document.createElement("option");
+			opt.innerHTML = "AM";
+			opt.value = 0;
+			select3.appendChild(opt);
+			opt = document.createElement("option");
+			opt.innerHTML = "PM";
+			opt.value = 1;
+			select3.appendChild(opt);			
+			
+			cell2.appendChild(select1);
+			cell2.innerHTML = cell2.innerHTML + ":";
+			cell2.appendChild(select2);
+			cell2.appendChild(select3);
+			
+			cell3.innerHTML = "and";
+			
+			var select4 = document.createElement("select");
+			for (var i = 1; i < 13; i++) {
+				var opt = document.createElement("option");
+				opt.innerHTML = i;
+				if (i == 12) {
+					opt.value = 0;
+				} else {
+					opt.value = i;
+				}
+				select4.appendChild(opt);
+			}
+			
+			var select5 = document.createElement("select");
+			for (var i = 0; i <= 55; i = i+5) {
+				var opt = document.createElement("option");
+				opt.innerHTML = i;
+				opt.value = i;
+				select5.appendChild(opt);
+			}
+			
+			var select6 = document.createElement("select");
+			var opt = document.createElement("option");
+			opt.innerHTML = "AM";
+			opt.value = "0";
+			select6.appendChild(opt);
+			opt = document.createElement("option");
+			opt.innerHTML = "PM";
+			opt.value = "1";
+			select6.appendChild(opt);
+			
+			cell4.appendChild(select4);
+			cell4.innerHTML = cell4.innerHTML + ":";
+			cell4.appendChild(select5);
+			cell4.appendChild(select6);
+			
+			var days = ["Mondays", "Tuesdays", "Wednesdays", "Thursdays", "Fridays", "Every day of the week"];
+			var vals = ["M", "T", "W", "R", "F", "MTWRF"];
+			var select7 = document.createElement("select");
+			for (var i = 0; i < 6; i++) {
+				var opt = document.createElement("option");
+				opt.innerHTML = days[i];
+				opt.value = vals[i];
+				select7.appendChild(opt);
+			}
+			cell5.innerHTML = cell5.innerHTML + "on ";
+			cell5.appendChild(select7);
+			
+			cell6.innerHTML = "<button type=\"button\" onclick=\"dropRange(this)\">Remove Range</button>";
+		}
+		function dropRange(button) {
+			var table = document.getElementById("schedPref");
+			var td = button.parentNode;
+			var tr = td.parentNode;
+			var ind = tr.rowIndex;
+			table.deleteRow(ind);
+		}
+		function setSchedValue() {
+			var sched = document.getElementById("sched");			
+			var table = document.getElementById("schedPref");
+			var rows = table.rows.length;
+			var row;
+			for (var i = 0; i < rows; i++) {
+			
+				row = table.rows[i];
+				var options = row.cells[1].getElementsByTagName("select");				
+				var begHour = options[0].value;
+				var begMin = options[1].value;
+				var begSuf = options[2].value;
+				if (begSuf == "1") {
+					begHour = parseInt(begHour) + 12;
+				}
+			
+				var options = row.cells[3].getElementsByTagName("select");				
+				var endHour = options[0].value;
+				var endMin = options[1].value;
+				var endSuf = options[2].value;
+				if (endSuf == "1") {
+					endHour = parseInt(endHour) + 12;
+				}
+			
+				var options = row.cells[4].getElementsByTagName("select");
+				var day = options[0].value;
+				
+				if (begMin < 10) {
+					begMin = "0" + begMin;
+				}
+				if (begHour < 10) {
+					begHour = "0" + begHour;
+				}
+				if (endMin < 10) {
+					endMin = "0" + endMin;
+				}
+				if (endHour < 10) {
+					endHour = "0" + endHour;
+				}
+				
+				sched.value = sched.value + " " + begHour + ":" + begMin + "-" + endHour + ":" + endMin + ":" + day + " ";
+			}
+		}
 	</script>
 	<?php
-		include('session_login_check.php');
 		include "DBaccess.php";
 		
 		$db = dbConnect();
-		$sql = "select distinct Department from Courses;";
-		$resultD = mysql_query($sql, $db);
-		$array = array();
-		while ($dep = mysql_fetch_array($resultD, MYSQL_BOTH)) {
-			$sql = sprintf("select Level,Title,CourseID from Courses where Department =\"%s\";", $dep[0]);
-			$resultL = mysql_query($sql, $db);
-			$array[$dep[0]] = array();
-			while ($lev = mysql_fetch_array($resultL, MYSQL_BOTH)) {
-				$array[$dep[0]][$lev[0]] = array($lev[0],$lev[1],$lev[2]);
-			}							
-		}
 		echo "<script>\n";
 		echo "function populateNum() {\n";
 		echo "var deps = document.getElementById(\"dep\");\n";
 		echo "var lvls = document.getElementById(\"level\");\n";
 		echo "var dep = deps.options[deps.selectedIndex].value;\n";
-		$keys1 = array_keys($array);
-		foreach ($keys1 as $key1) {
-			printf("if (dep == \"%s\") {\n", $key1);
-			echo "\tlvls.options.length=0;\n";
+		
+		$sql = "select distinct Department from Courses;";
+		$resultD = mysql_query($sql, $db);
+		while ($dep = mysql_fetch_array($resultD)) {
+			printf("if (dep == \"%s\") {\n", $dep[0]);
+			printf("\tlvls.options.length=0;\n");
 			$i = 0;
-			$keys2 = array_keys($array[$key1]);
-			foreach ($keys2 as $key2) {				
-				printf("\tlvls.options[%d]=new Option(\"%s - %s\", \"%s\", false, false);\n", $i++, $array[$key1][$key2][0], $array[$key1][$key2][1], $array[$key1][$key2][2]);
-			}
-			echo "}\n";
+			
+			$sql = sprintf("select Level,Title,CourseID from Courses where Department=\"%s\" order by Level;", $dep[0]);
+			$resultL = mysql_query($sql, $db);
+			while ($lev = mysql_fetch_array($resultL)) {
+				$level = $lev[0];
+				$title = $lev[1];
+				$cid = $lev[2];
+				printf("\tlvls.options[%d]=new Option(\"%s - %s\", \"%s\", false, false);\n", $i++, $level, $title, $cid);
+			}		
+			printf("}\n");
 		}
-		echo "}</script>";
+		printf("}</script>");
 		
 	?>
 	<body>
@@ -210,161 +368,17 @@
 							</select>
 						</td>
 					</tr>
-					<!--
-							<tr>
-								<td>
-									Number of Major Courses:
-								</td>
-								<td>
-									Between: 
-									<select name="numMajCoursesMin" id="numMajCoursesMin">
-										<option value="0">0</option>
-										<option value="1">1</option>
-										<option value="2">2</option>
-										<option value="3">3</option>
-										<option value="4">4</option>
-										<option value="5">5</option>
-										<option value="6">6</option>
-									</select>
-									and: 
-									<select name="numMajCoursesMax" id="numMajCoursesMax">
-										<option value="0">0</option>
-										<option value="1">1</option>
-										<option value="2">2</option>
-										<option value="3">3</option>
-										<option value="4">4</option>
-										<option value="5">5</option>
-										<option value="6">6</option>
-									</select>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									Number of GenEd Courses:
-								</td>
-								<td>
-									Between: 
-									<select name="numGenCoursesMin" id="numGenCoursesMin">
-										<option value="0">0</option>
-										<option value="1">1</option>
-										<option value="2">2</option>
-										<option value="3">3</option>
-										<option value="4">4</option>
-										<option value="5">5</option>
-										<option value="6">6</option>
-									</select>
-									and: 
-									<select name="numGenCoursesMax" id="numGenCoursesMax">
-										<option value="0">0</option>
-										<option value="1">1</option>
-										<option value="2">2</option>
-										<option value="3">3</option>
-										<option value="4">4</option>
-										<option value="5">5</option>
-										<option value="6">6</option>
-									</select>
-								</td>
-							</tr>
-					-->
 				</table>						
 				<br />
 				<br />
 				<h2>Schedule Preferences:</h2>
-				<table border="0">
-					<tr>
-						<td>
-							No class between:
-						</td>
-						<td>
-							<select name="begHour" id="endHour">
-								<?php
-									for ($i = 0; $i < 24; $i++) {
-										printf("<option value=\"%d\">%02d</option>", $i, $i);
-									}
-								?>
-							</select>
-							:
-							<select name="begMin" id="begMin">
-								<?php
-									for ($i = 0; $i < 60; $i++) {
-										printf("<option value=\"%d\">%02d</option>", $i, $i);
-									}
-								?>
-							</select>
-							 and 
-							<select name="endHour" id="endHour">
-								<?php
-									for ($i = 0; $i < 24; $i++) {
-										printf("<option value=\"%d\">%02d</option>", $i, $i);
-									}
-								?>
-							</select>
-							:
-							<select name="endMin" id="endMin">
-								<?php
-									for ($i = 0; $i < 60; $i++) {
-										printf("<option value=\"%d\">%02d</option>", $i, $i);
-									}
-								?>
-							</select>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							No class before:
-						</td>
-						<td>
-							<select name="earlyHour" id="earlyHour">
-								<?php
-									for ($i = 0; $i < 24; $i++) {
-										printf("<option value=\"%d\">%02d</option>", $i, $i);
-									}
-								?>
-							</select>
-							:
-							<select name="earlyMin" id="earlyMin">
-								<?php
-									for ($i = 0; $i < 60; $i++) {
-										printf("<option value=\"%d\">%02d</option>", $i, $i);
-									}
-								?>
-							</select>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							No class after:
-						</td>
-						<td>
-							<select name="lateHour" id="lateHour">
-								<?php
-									for ($i = 0; $i < 24; $i++) {
-										printf("<option value=\"%d\">%02d</option>", $i, $i);
-									}
-								?>
-							</select>
-							:
-							<select name="lateMin" id="lateMin">
-								<?php
-									for ($i = 0; $i < 60; $i++) {
-										printf("<option value=\"%d\">%02d</option>", $i, $i);
-									}
-								?>
-							</select>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							Prefer classes with friends:
-						</td>
-						<td>
-							<input type="checkbox" name="preferFriends" id="preferFriends">
-						</td>
-					</tr>
+				<button type="button" onclick="addRange()">Add Range</button>
+				<table name="schedPref" id="schedPref" border="0">
 				</table>
 				<input type="submit" name="Submit" value="Submit" />
 				<input type="hidden" name="groupsInput" id="groupsInput" value="" />
 				<input type="hidden" name="wantedClasses" id="wantedClasses" value="" />
+				<input type="hidden" name="sched" id="sched" value="" />
 			</form>
 			<form action="home.php"><input type="submit" value="Go Back"></form>
 		</div>
