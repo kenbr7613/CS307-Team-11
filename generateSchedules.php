2ab25a49b903e62ca8c00480d7f0500efe48ddb8
bpastene <bpastene@purdue.edu>
2013-11-29 14:31:27 -0800

    Update generateSchedules.php

--- generateSchedules.php
+++ generateSchedules.php
@@ -1,47 +1,10 @@
 <?php
 	include('session_login_check.php');
-	include('UserSchedule.php');
+	include('linked_sections.php');
+	include('getWalkTime.php');
 	
 	$builtSched = 0;
 	$scheds = array();
-	
-	function getLinkedClasses($courseID) {
-		global $db;
-		//query to start linked section search(start from Lectures, Individual Studies, and Labratory Preparation
-		$query_for_primary_classes = sprintf("SELECT CRN, LinkID, LinkedSection, CourseType  FROM CourseOfferings Where CourseID=%d AND (CourseType=3 OR CourseType=5 OR CourseType=6);", $courseID);
-		$main_classes = mysql_query($query_for_primary_classes, $db);
-		$result_array = array();
-		//loop through all main classes and look for linked sections
-		for ($i = 0; $i < mysql_num_rows($main_classes); $i++) {
-			$row = mysql_fetch_assoc($main_classes);
-			$LinkID_primary = $row["LinkID"];
-			$LinkedSection_primary = $row["LinkedSection"];
-			$CRN_primary = $row["CRN"];
-			//Contains ALL classes not a lecture, IS, or Lab Prep associated with our CourseID
-			$query = sprintf("SELECT CRN, LinkID, LinkedSection, CourseType  FROM CourseOfferings Where LinkID='%s' AND CourseID=%d AND (CourseType!=3 OR CourseType!=5 OR CourseType!=6);", 					$LinkedSection_primary, $courseID);
-			$secondary_classes = mysql_query($query, $db) or die($query."<br/><br/>".mysql_error());
-			for ($j = 0; $j < mysql_num_rows($secondary_classes); $j++) {
-				$row2 = mysql_fetch_assoc($secondary_classes);
-				$class_array = array();
-				//add our lecture to the array
-				$class_array[] = $CRN_primary;
-				//insert into class array
-				$class_array[] = $row2["CRN"];
-				$LinkedSection_n = $row2["LinkedSection"];
-				while ($LinkedSection_n != $LinkID_primary) {
-					//continue going through the sections
-					$linked_query = sprintf("SELECT CRN, LinkID, LinkedSection, CourseType  FROM CourseOfferings Where LinkID='%s' AND CourseID=%d AND (CourseType!=3 OR CourseType!=5 OR CourseType!=6);", 					$LinkedSection_n, $courseID);
-					$n_classes = mysql_query($linked_query, $db) or die($linked_query."<br/><br/>".mysql_error());
-					$obj = mysql_fetch_assoc($n_classes);
-					$class_array[] = $obj["CRN"];
-					$LinkedSection_n = $obj["LinkedSection"];
-				}
-				$result_array[] = $class_array;
-			}
-		}
-		
-		return $result_array;
-	}
 	function lessThan($time1, $time2) {
 		$time1 = str_replace(":", "", $time1);
 		$time2 = str_replace(":", "", $time2);
@@ -72,7 +35,7 @@
 			return false;
 		}
 	}
-	function sameDay($days1, $days2) {
+	function sameDays($days1, $days2) {
 		$days1Array = str_split($days1);
 		$days2Array = str_split($days2);		
 		$commonDays = array_intersect($days1Array, $days2Array);
@@ -87,7 +50,7 @@
 			$front = $time[0];
 			$back = $time[1];
 			$daysT = $time[2];
-			if (sameDay($days, $daysT)) {
+			if (sameDays($days, $daysT)) {
 				if (!((lessThan($start, $front) && lessThan($end, $front)) || (greaterThan($start, $back) && greaterThan($end, $back)))) {
 					// conflict
 					return false;
@@ -96,14 +59,13 @@
 		}
 		return true;
 	}
-	function friendsIn($sections) {
+	function numberOfFriends($section) {
 		global $friendsSections;
-		foreach ($sections as $section) {
-			if (in_array($section, $friendsSections)) {
-				return 1;
-			}
+		$friends = 0;
+		if (isset($friendsSections[$section])) {
+			$friends = $friendsSections[$section];
 		}
-		return 0;
+		return $friends;
 	}
 	function buildSchedule ($courses, $sections, $times) {
 		global $builtSched;
@@ -132,10 +94,17 @@
 			if (!$isConflict) {
 				$totalBuilt++;				
 				$builtSched = 1;
-				if (friendsIn($sections)) {
-					array_unshift($scheds, $sections);
-				} else {
-					array_push($scheds, $sections);
+				$prevSection = "";
+				$scheds[$totalBuilt-1] = array();
+				foreach ($sections as $section) {
+					$friends = numberOfFriends($section);
+					if ($prevSection = "") {
+						$walking = 0;
+					} else {
+						$walking = getWalkTime($prevSection, $section);
+					}
+					array_push($scheds[$totalBuilt-1], array($section, $friends, $walking));
+					$prevSection = $section;
 				}
 			}		
 		} else {
@@ -163,7 +132,11 @@
 	$sql = sprintf("select CourseOfferingID from UserSchedule where UserID in (%s);", implode(",", $friends));
 	$result = mysql_query($sql);
 	while ($row = mysql_fetch_array($result)) {
-		array_push($friendsSections, $row[0]);
+		if (isset($friendsSections[$row[0]])) {
+			$friendsSections[$row[0]] = 0;
+		} else {
+			$friendsSections[$row[0]]++;
+		}
 	}
 	
 	$sets = $_SESSION['setsOfCourses'];
