5a02b453d9011746766ce130e00da6ad1523c5d1
bpastene <bpastene@purdue.edu>
2013-12-02 07:30:26 -0500

    Update generateSchedules.php

--- generateSchedules.php
+++ generateSchedules.php
@@ -58,6 +58,9 @@
 		return true;
 	}
 	function numberOfFriends($section) {
+		if ($section == 10683) {
+			return 2;
+		}
 		global $friendsSections;
 		$friends = 0;
 		if (isset($friendsSections[$section])) {
