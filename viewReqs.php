33e40a71443676f33d3c34f1f5aaf96c5b45673f
kenbr7613 <kenneth.lloyd.brown@gmail.com>
2013-12-01 23:06:14 -0500

    Create viewReqs.php

---
+++ viewReqs.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<title>Requirements</title>
+</head>
+<body>
+<center>
+<h2>Requirements</h2>
+<?php
+	if($_GET['Flag'] == 0)
+		$req = "All";
+	else
+		$req = "Pick " . $row['Flag'];
+	printSubGroupTable($_GET['GroupID'], $_GET['Desc'], $req);
+?>
+</center>
+</body>
+</html>
+<?php
+
+
+	function printSubGroupTable($groupID,$desc, $numReq)
+	{
+		mysql_connect('lore.cs.purdue.edu:11394', 'root', 'cs307team11');
+		mysql_select_db("purduePlannerDB"); 
+
+		echo "<p><table border=3 cellpadding=5 cellspacing=0 bordercolor=#000000>\r\n";
+		echo "<tr><td><b>" . $desc . " - " . $numReq . "</b></td></tr>\r\n";
+		$sql = "SELECT * FROM RequirementGroups WHERE GroupID = " . $groupID;
+		$result = mysql_query($sql);
+		while ($row = mysql_fetch_array($result)) {
+			if($row['Flag'] != 9999)
+			{
+				echo "<tr><td>";
+				if($row['Flag'] == 0)
+					$req = "All";
+				else
+					
